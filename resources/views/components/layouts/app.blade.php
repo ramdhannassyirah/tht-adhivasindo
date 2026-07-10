59b431ef780c577db54495c8c7088455cf62c161
Ramdhan Nassyirah <ramdhannassyirahh@gmail.com>
2026-07-10 18:21:03 +0700

    fix: layout app ui

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -31,20 +31,50 @@
             @if (Route::has('login'))
                 <nav x-data="{ isLogin: localStorage.getItem('token'), user: JSON.parse(localStorage.getItem('user')) }" class="flex items-center gap-4">
 
+                    <template x-if="isLogin">
+                        <div class="flex items-center gap-2">
+                            <p x-text="user.name" class="px-4 py-2 bg-gray-100 rounded-lg text-sm text-gray-700"></p>
+                            <template x-if="user.role === 'customer'" class="relative">
+                                <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    viewBox="0 0 24 24">
+                                    <path d="M0 0h24v24H0z" fill="none" />
+                                    <g fill="none" stroke="currentColor" stroke-width="1.5">
+                                        <path
+                                            d="M3.864 16.455c-.858-3.432-1.287-5.147-.386-6.301S6.148 9 9.685 9h4.63c3.538 0 5.306 0 6.207 1.154s.472 2.87-.386 6.301c-.546 2.183-.818 3.274-1.632 3.91c-.814.635-1.939.635-4.189.635h-4.63c-2.25 0-3.375 0-4.189-.635c-.814-.636-1.087-1.727-1.632-3.91Z" />
+                                        <path
+                                            d="m19.5 9.5l-.71-2.605c-.274-1.005-.411-1.507-.692-1.886A2.5 2.5 0 0 0 17 4.172C16.56 4 16.04 4 15 4M4.5 9.5l.71-2.605c.274-1.005.411-1.507.692-1.886A2.5 2.5 0 0 1 7 4.172C7.44 4 7.96 4 9 4" />
+                                        <path d="M9 4a1 1 0 0 1 1-1h4a1 1 0 1 1 0 2h-4a1 1 0 0 1-1-1Z" />
+                                    </g>
+                                </svg>
+                                <span>
+                                    <template x-if="cart_count > 0">
+                                        <span class="absolute -top-2 -right-1 w-3 h-3 bg-red-500 rounded-full"></span>
+                                    </template>
+                                </span>
+                            </template>
+                        </div>
+                    </template>
+
                     <template x-if="isLogin && user.role === 'admin'">
                         <div class="flex items-center gap-4">
                             <a href="/dashboard"
-                                class="px-4 py-2 rounded-lg bg-blue-500 text-white text-sm hover:bg-blue-600 transition">
+                                class="px-4 py-2 bg-gray-700 rounded-lg text-sm text-gray-100 transition">
                                 Dashboard
                             </a>
-                           
+
                         </div>
                     </template>
+
                     <template x-if="isLogin">
-                             <a href="#" @click.prevent="logout()"
-                                class="px-4 py-2 rounded-lg bg-red-500 text-white text-sm hover:bg-red-600 transition">
-                                Logout
-                            </a>
+                        <a href="#" @click.prevent="logout()"
+                            class="  text-red-500 hover:text-red-700 transition">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24">
+                                <path d="M0 0h24v24H0z" fill="none" />
+                                <path fill="currentColor"
+                                    d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h7v2zm11-4l-1.375-1.45l2.55-2.55H9v-2h8.175l-2.55-2.55L16 7l5 5z" />
+                            </svg>
+
+                        </a>
                     </template>
 
                     <template x-if="!isLogin">
@@ -60,7 +90,6 @@
         </div>
     </header>
 
-    <!-- Content -->
     <main class="">
         <div class="">
             {{ $slot }}
@@ -68,7 +97,6 @@
     </main>
 
 
-    <!-- FOOTER -->
     <footer class="bg-gray-800 text-gray-300 py-6 text-center">
         <p>&copy; 2026 Toko Sederhana. All rights reserved.</p>
     </footer>
